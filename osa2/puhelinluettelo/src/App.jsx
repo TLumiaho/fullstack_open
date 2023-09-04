8c68eba3d2a08177c5ca424f11c780fb7a43c54f
TLumiaho <tuomas.lumiaho@hotmail.com>
2023-09-04 13:40:58 +0300

    tehtävä 2.6.

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { key: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      key: newName
+    }
+    setPersons(persons.concat(nameObject))
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person =>
+        <p>{person.key}</p>
+      )}
+    </div>
+  )
+
+}
+
+export default App
